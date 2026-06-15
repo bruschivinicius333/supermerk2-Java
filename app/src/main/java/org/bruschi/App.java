91b52cd8fbd7792818164420c0b3bd0fd23bb284
bruschivinicius333 <bruschivinicius333@gmail.com>
2026-06-15 16:03:15 -0300

    whurever

--- app/src/main/java/org/bruschi/App.java
+++ app/src/main/java/org/bruschi/App.java
@@ -3,12 +3,12 @@
  */
 package org.bruschi;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+public class App {    
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        
+        System.out.println("dgwgweh");
+        
     }
+    
 }
